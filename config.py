4f0009d297f5e9f6f6abc3ab72410119ddbad703
Levancuong1 <lecuongnso@gmail.com>
2026-05-30 23:24:07 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,25 @@
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 BLOCK_SIZE = 20
+FPS = 60
 
-COLOR_BLACK = (0, 0, 0)
-COLOR_DARK_GRAY = (15, 15, 15)
-COLOR_WHITE = (255, 255, 255)
-COLOR_GREEN = (46, 204, 113)
-COLOR_DARK_GREEN = (39, 174, 96)
-COLOR_RED = (231, 76, 60)
-COLOR_YELLOW = (241, 196, 15)
+COLOR_BLACK = (10, 10, 12)       
+COLOR_GRID = (25, 25, 30)        
+COLOR_WHITE = (240, 240, 245)
+COLOR_RED = (255, 65, 54)        
+COLOR_YELLOW = (255, 215, 0)
+COLOR_GRAY = (120, 120, 125)
+COLOR_GREEN = (0, 242, 125)
+COLOR_DARK_GRAY = (25, 25, 30)
 
-INITIAL_SPEED = 10
+SKINS = [
+    {"name": "Cyberpunk", "head": (0, 242, 125), "tail": (0, 115, 60)},
+    {"name": "Hỏa Thần", "head": (255, 65, 54), "tail": (150, 20, 10)},
+    {"name": "Thủy Quái", "head": (0, 180, 255), "tail": (0, 50, 150)},
+    {"name": "Hoàng Kim", "head": (255, 215, 0), "tail": (180, 120, 0)}
+]
+
+INITIAL_SPEED = 8
 SPEED_INCREMENT = 1
 SCORE_INCREMENT = 10
+TIME_ATTACK_LIMIT = 6000
